461d4cd32ce024769a83354dace0b830b4f25bde
igorsok <>
2011-05-05 22:50:50 +0000

    Improve comments

--- src/PIC_ModParticles.f90
+++ src/PIC_ModParticles.f90
@@ -1,17 +1,22 @@
 !^CFG COPYRIGHT UofM
 module PIC_ModParticles
-  use PIC_ModGrid,ONLY:nSortParticle,nElectronMax
-  use PIC_ModMain,ONLY:SaveVelocity,UseBunemanScheme
+  use PIC_ModGrid,ONLY:nSortParticle, nElectronMax
+  use PIC_ModMain,ONLY:SaveVelocity
   use PIC_ModMain,ONLY:c,c2,Dt,Dx_D,CellVolume
-  use PIC_ModParticleInField
+  use PIC_ModParticleInField,ONLY: rho_G,add_density,add_current
+  use PIC_ModParticleInField,ONLY: b_interpolated_d,e_interpolated_d
+  use PIC_ModParticleInField,ONLY: HighFF_ID, HighFFNew_ID,&
+                              Node_D,NodeNew_D 
   use PIC_ModProc,ONLY:iProc,iError
   use PIC_ModMpi
   implicit none
   integer,parameter::W_=nDim,Wx_=W_+x_,Wy_=W_+y_,Wz_=W_+z_
+
   integer,parameter::Electrons_=1,Electron_=Electrons_
+
   !Structures
   real,dimension(nSortParticle)::M_P,Q_P
-  !Particle's mass M_P, dt*Q_P, dt*Q_P/M_P
+  !Particle's mass M_P, dt*Q_P
   type particles
      real,dimension(:,:),pointer::Coords
      !Particle coordinates in the phase space. First nDim components 
@@ -74,40 +79,65 @@ contains
     Of(iSort)%Coords(x_:nDim,n_P(iSort))=PhaseCoords_D
   end subroutine put_particle
   !--------------------------------------------------------------!
+  !================PARTICLE MOVER================================!
   subroutine advance_particles(iSort)
+
     integer,intent(in)::iSort
+
     real::M,QDt,QcDtPerV,QDtPerM,Energy,W2
+
     integer::iParticle
     real,dimension(nDim)::X_D,QPerVDx_D
+
     real,dimension(x_:z_)   ::W_D,W12_D,EForce_D,BForce_D
     real::Gamma
-    real,dimension(:,:),pointer::CoordPhaseSp
+    real,dimension(:,:),pointer::Coord_VI
+    !-------------------------------
+    !    Initialize the simulation for this sort of particles
+    !Mass 
     M=M_P(iSort)
+
+    !Charge times Dt
     QDt=Dt*Q_P(iSort)
+
+    !Q * dt * c / V (dx*dy*dz)
     QcDtPerV=QDt*c/CellVolume
+
+    !1/2 * Q * dt / M 
     QDtPerM=cHalf*QDt/M
+
+    !Q / V * (/\Delta x, \Delta y, \Delta z/)
     QPerVDx_D=Q_P(iSort)/CellVolume*Dx_D
+
+    !Nullify counters: for energy:
     Energy_P(iSort)=cZero
     rho_G=cZero
-    call set_pointer_to_particles(iSort,CoordPhaseSp)
+
+    call set_pointer_to_particles(iSort,Coord_VI)
+
+    !Start the loop over particles
     do iParticle=1,n_P(iSort)
-       X_D=CoordPhaseSp(x_:nDim,iParticle)
-       W_D=CoordPhaseSp(Wx_:Wz_,iParticle)
-       if(SaveVelocity)then
-          Gamma=cOne/sqrt(c2-sum(W_D**2))
-          W_D=Gamma*c*W_D
-          W2=Gamma**2-c2
-       else
-          W2=sum(W_D**2)
-          Gamma=sqrt(c2+W2)
-       end if
+       !Get coordinates and velocities!
+       X_D=Coord_VI(x_:nDim,iParticle)
+       W_D=Coord_VI(Wx_:Wz_,iParticle)
+
+!       if(SaveVelocity)then
+!          Gamma=cOne/sqrt(c2-sum(W_D**2))
+!          W_D=Gamma*c*W_D
+!          W2=Gamma**2-c2
+!       else
+
+       W2 = sum(W_D**2)
+       Gamma = sqrt(c2 + W2)
+
+!       end if
        !Now W_D is the initial momentum, W2=W_D^2
        !Mow Gamma is the initial Gamma-factor multiplied by c
 
        call get_form_factors(X_D,Node_D,HighFF_ID)
 
        !Electric field force
-       EForce_D=QDtPerM*e_interpolated_d()
+       EForce_D = QDtPerM*e_interpolated_d()
 
        !Add kinetic energy
 
@@ -117,15 +147,15 @@ contains
        !Acceleration from the electric field, for the
        !first half of the time step:
 
-       W_D=W_D+EForce_D
+       W_D = W_D + EForce_D
 
        !Get magnetic force
 
-       BForce_D=(QDtPerM/sqrt(c2+sum(W_D**2)))*b_interpolated_d()
+       BForce_D=QDtPerM/sqrt( c2+sum(W_D**2) ) *b_interpolated_d()
 
        !Add a half of the magnetic rotation:
 
-       W12_D=W_D+cross_product(W_D,BForce_D)
+       W12_D = W_D + cross_product(W_D,BForce_D)
 
        !Multiply the magnetic force by 2 to take a whole
        !rotation and reduce its magnitude not to perturb energy
@@ -139,42 +169,55 @@ contains
        Gamma=sqrt(c2+sum(W_D**2))
        !Mow Gamma is the final Gamma-factor multiplied by c
 
-       if(SaveVelocity)then
-          W_D=(cOne/Gamma)*W_D
-          CoordPhaseSp(Wx_:Wz_,iParticle)=c*W_D
-       else
-          CoordPhaseSp(1+nDim:3+nDim,iParticle)=W_D
-          W_D=(cOne/Gamma)*W_D
-       end if
+       !if(SaveVelocity)then
+       !   W_D=(cOne/Gamma)*W_D
+       !   Coord_VI(Wx_:Wz_,iParticle)=c*W_D
+       !else
+
+       !Save momentum
+       Coord_VI(1+nDim:3+nDim,iParticle)=W_D
+       W_D=(cOne/Gamma)*W_D
+       
+       !end if
+
        !Now W_D is the velocity divided by c
        !Update coordinate
        X_D=X_D+SpeedOfLight_D*W_D(1:nDim)
-       CoordPhaseSp(1:nDim,iParticle)=X_D
+       Coord_VI(1:nDim,iParticle)=X_D
 
        !New form factor
        call get_form_factors(X_D,NodeNew_D,HighFFNew_ID)
+ 
+       !Contribute to the charge densiry
        call add_density(NodeNew_D,HighFFNew_ID)
-       if(UseBunemanScheme)CYCLE
-       if(nDim<3)then
-          W_D=QcDtPerV*W_D !For nDim=3 velocity is not used
-          call add_current(QPerVDx_D,W_D)
-       else
-          call add_current(QPerVDx_D)
-       end if
+   
+ !      if(nDim<3)then
+ !         W_D=QcDtPerV*W_D !For nDim=3 velocity is not used
+ !         call add_current(QPerVDx_D,W_D)
+ !       else
+
+       call add_current(QPerVDx_D)
+
+ !      end if
     end do
+
     call pass_density
+
     if(iProc==0)then
        call get_rho_max(OmegaPDtMax_P(iSort))
        OmegaPDtMax_P(iSort)=sqrt(QDt**2/M*OmegaPDtMax_P(iSort))
     end if
   contains
+    !========================
     function cross_product(a,b)
       real,dimension(3)::cross_product
       real,dimension(3),intent(in)::a,b
+      !----------------
       cross_product(1)=a(2)*b(3)-a(3)*b(2)
       cross_product(2)=a(3)*b(1)-a(1)*b(3)
       cross_product(3)=a(1)*b(2)-a(2)*b(1)
     end function cross_product
+    !=======================
   end subroutine advance_particles
   !--------------------------------------------------------------!
 end module PIC_ModParticles
