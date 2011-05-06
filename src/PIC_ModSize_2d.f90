1a196a0d74b4673a36775c7337e65fa6fdd7d1e4
igorsok <>
2011-05-06 16:47:22 +0000

    Renaming

--- src/PIC_ModSize_2d.f90
+++ src/PIC_ModSize_2d.f90
@@ -4,5 +4,5 @@ module PIC_ModGrid
   integer,parameter::nX=10,nY=10       !The numbers of the grid 
                                        !cells
   integer,parameter::x_=1,y_=2,z_=3
-  integer,parameter::nSortParticle=2,nElectronMax=10000000
+  integer,parameter::nPType=2,nElectronMax=10000000
 end module PIC_ModGrid
