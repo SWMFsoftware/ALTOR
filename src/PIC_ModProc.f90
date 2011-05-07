a8da967bccd8c8ab98d72fbbcb7b5f2f1838f07e
igorsok <>
2011-05-07 14:55:36 +0000

    Cleanup

--- src/PIC_ModProc.f90
+++ src/PIC_ModProc.f90
@@ -1,27 +1,12 @@
 module PIC_ModProc
-  use ModMpi
   implicit none
+  SAVE
+
   integer::iProc !PE rank
   integer::nProc !Number of PEs
   integer::iComm !Communicator
   integer::iError!Auxiliary integer to store an error number
-  !Methods
-  public::init_mpi !Intitialize MPI, iProc,nProc,iComm
-  public::clean_mpi
-contains
-!-----------------------------------------------------------!
-  subroutine init_mpi
-    call MPI_INIT(iError)
-    iComm=MPI_COMM_WORLD
-    call MPI_COMM_RANK(iComm,iProc,iError)
-    call MPI_COMM_SIZE(iComm,nProc,iError)
-  end subroutine init_mpi
-!-----------------------------------------------------------!
-  subroutine clean_mpi
-    call MPI_Finalize(iError)
-    stop
-  end subroutine clean_mpi
-!-----------------------------------------------------------!
+
 end module PIC_ModProc
     
     
