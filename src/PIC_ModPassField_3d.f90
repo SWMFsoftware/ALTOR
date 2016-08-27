377bad585c21cd4b5dc4ea77016834e777b2dbfa
hyzhou <>
2016-08-27 00:57:02 +0000

    add subroutine pass_velocity for applying periodic BC. and collecting info from all processors for velocities.

--- src/PIC_ModPassField_3d.f90
+++ src/PIC_ModPassField_3d.f90
@@ -16,7 +16,7 @@ module PIC_ModMpi
   integer,parameter::iBuffSizeMax=10 !In MByte
   integer,parameter,private::KByte=1024, M=KByte*KByte
   !Methods:
-  public::pass_density
+  public::pass_density, pass_velocity
   public::pass_current
 contains
   subroutine pass_density(iProcIn)
@@ -25,7 +25,8 @@ contains
        iBuffSizeMax*M/(4*(iRealPrec+1)*(nX+2*iGCN)*(nY+2*iGCN)),1)
     real,dimension(1-iGCN:nX+iGCN,1-iGCN:nY+iGCN,iLength)::Buff_G
     integer::k,kNew
-    !--------------
+    !-----------------------------------------------------------
+    !Set up periodic BC in all three dimensions.
     call density_bc_periodic(1)
     if(nProc==1)return
     !If iProcIn is given, the result is at PE=iProcIn only
@@ -59,7 +60,51 @@ contains
        end do
     end if
   end subroutine pass_density
-  !---------------------------------------------------------------!
+  !==============================================================
+  !Test
+  subroutine pass_velocity(iProcIn)
+    integer,optional::iProcIn
+    integer,parameter::iLength=max(&
+         iBuffSizeMax*M/(4*(iRealPrec+1)*(nX+2*iGCN)*(nY+2*iGCN)),1)
+    real,dimension(1:3,1-iGCN:nX+iGCN,1-iGCN:nY+iGCN,iLength)::Buff_G
+    integer::k,kNew
+    !-----------------------------------------------------------
+    !Set up periodic BC in all three dimensions.
+    call velocity_bc_periodic(1)
+    if(nProc==1)return
+    !If iProcIn is given, the result is at PE=iProcIn only
+    if(present(iProcIn))then
+       k=-iGCN
+       do while(k<nZ+iGCN)
+          kNew=min(nZ+iGCN,k+iLength)
+          call MPI_REDUCE(&
+               V_GDB(:,1-iGCN,1-iGCN,k+1,1),&
+               Buff_G(:,1-iGCN,1-iGCN,1),&
+               3*(nX+2*iGCN)*(nY+2*iGCN)*(kNew-k),&
+               MPI_REAL,&
+               MPI_SUM,&
+               iProcIn,iComm,iError)
+          if(iProc==iProcIn)&
+             V_GDB(:,:,:,k+1:kNew,1) = Buff_G(:,:,:,1:kNew-k)
+          k=kNew
+       end do
+    else
+       k=-iGCN
+       do while(k<nZ+iGCN)
+          kNew=min(nZ+iGCN,k+iLength)
+          call MPI_ALLREDUCE(&
+               V_GDB(:,1-iGCN,1-iGCN,k+1,1),&
+               Buff_G(:,1-iGCN,1-iGCN,1),&
+               3*(nX+2*iGCN)*(nY+2*iGCN)*(kNew-k),&
+               MPI_REAL,&
+               MPI_SUM,&
+               iComm,iError)
+          V_GDB(:,:,:,k+1:kNew,1)=Buff_G(:,:,:,1:kNew-k)
+          k=kNew
+       end do
+    end if
+  end subroutine pass_velocity
+  !=============================================================
   subroutine pass_current
     integer,parameter::iLength=max(&
        iBuffSizeMax*M/(4*(iRealPrec+1)*(nX+2*iGCN)*(nY+2*iGCN)),1)
