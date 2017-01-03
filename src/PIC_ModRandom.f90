91fee0ce44fcef2dbf1d46312df28d16c369f1a6
igorsok <>
2017-01-03 04:53:00 +0000

    Change iSeed=>1Seed_I(1:10), which allows us to initialize mutiple sequencies of pseudo-random numbers (say, to gamble particle coordinates and particle momentum, using two different sequancies for this)

--- src/PIC_ModRandom.f90
+++ src/PIC_ModRandom.f90
@@ -6,25 +6,38 @@
 module PIC_ModRandom
   implicit none
   private !Except
-  integer::iSeed=0
+  integer::iSeed_I(10)=0
   !Methods
   public::rand
   public::init_rand
 contains
-  subroutine init_rand(iSeedIn)
-    integer,optional,intent(in)::iSeedIn
-    if(present(iSeedIn))then
-       iSeed=iSeedIn
+  subroutine init_rand(iSeedIn,iIndexIn)
+    integer,optional,intent(in)::iSeedIn,iIndexIn
+    integer:: iIndex
+    if(present(iIndexIn))then
+       iIndex = iIndexIn
     else
-       iSeed=1
+       iIndex = 1
+    end if
+    if(present(iSeedIn))then
+       iSeed_I(iIndex) = iSeedIn
+    else
+       iSeed_I(iIndex)=1
     end if
   end subroutine init_rand
   !-----------------------------------------------------------------!
   !===============================================================C
-  REAL FUNCTION RAND()
-    ISEED=ISEED*48828125
-    IF(ISEED < 0) ISEED=(ISEED+2147483647)+1
-    if(iSeed==0) iSeed=1
-    RAND=FLOAT(ISEED)/2147483647
+  REAL FUNCTION RAND(iIndexIn)
+    integer,optional,intent(in)::iIndexIn
+    integer:: iIndex
+    if(present(iIndexIn))then
+       iIndex = iIndexIn
+    else
+       iIndex = 1
+    end if
+    ISEED_I(iIndex)=ISEED_I(iIndex)*48828125
+    IF(ISEED_I(iIndex) < 0) ISEED_I(iIndex)=(ISEED_I(iIndex)+2147483647)+1
+    if(iSeed_I(iIndex)==0) iSeed_I(iIndex)=1
+    RAND=FLOAT(ISEED_I(iIndex))/2147483647
   END FUNCTION RAND
 end module PIC_ModRandom
