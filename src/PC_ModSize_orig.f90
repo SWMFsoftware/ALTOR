a750bb35f8f116d8d84cbb1f64a94c65c0d1cf6a
gtoth <>
2017-11-25 21:28:38 +0000

    Now MaxBlock is taken from PC_BATL_size. This hopefully fixes the compilation issue with ifort on Pleiades. This is a compiler bug, but it is better to have an adjustable MaxBlock that is the same in BATL and in ALTOR than the current complicated distinction. Same change was done in BATSRUS.

--- src/PC_ModSize_orig.f90
+++ src/PC_ModSize_orig.f90
@@ -5,9 +5,8 @@
 module PC_ModSize
   use PC_BATL_size, ONLY: nX=>nI, nY=>nJ, nZ=>nK, jDim_, kDim_
   use PC_BATL_size, ONLY: nPType=>nKindParticle, nDim, MaxDim
-  use PC_BATL_size, ONLY:nBlock
-  integer, parameter :: x_=1,y_=2,z_=3
-  integer, parameter :: MaxBlock=1
+  use PC_BATL_size, ONLY: nBlock, MaxBlock
+  integer, parameter :: x_=1, y_=2, z_=3
   integer, parameter :: nElectronMax=10000000
   integer, parameter :: nCell_D(MaxDim) = (/nX, nY, nZ/)
 
